37693f51779a8568e2f68d531afa8de563381354
Kristina <krissss.es.369@gmail.com>
2026-09-07 02:49:50 +0700

    Добавлен метод draw

--- task1/main.py
+++ task1/main.py
@@ -4,6 +4,9 @@ class Cat:
         self.name = name
         self.age = age
 
+    def draw(self):
+        print(f"На экране рисуется кот {self.name}, порода {self.breed}.")
+
 
 cat1 = Cat("Британская", "Мурзик", 3)
 cat2 = Cat("Сиамская", "Луна", 2)
@@ -13,3 +16,7 @@ cat3 = Cat("Мейн-кун", "Барсик", 5)
 print(cat1.breed, cat1.name, cat1.age)
 print(cat2.breed, cat2.name, cat2.age)
 print(cat3.breed, cat3.name, cat3.age)
+
+cat1.draw()
+cat2.draw()
+cat3.draw()
